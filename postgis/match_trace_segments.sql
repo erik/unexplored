67e9a7c3d5d1c9352a98485e4ce7fd79d4cabb3c
Erik Price <github@erikprice.net>
2021-03-27 22:15:20 -0700

    Fix match_trace_segments query

--- postgis/match_trace_segments.sql
+++ postgis/match_trace_segments.sql
@@ -4,21 +4,28 @@ DROP TABLE IF EXISTS path_segment_traces CASCADE;
 
 -- TODO: Spend more time on the EXPLAIN
 CREATE TABLE path_segment_traces AS
-  WITH within_bounds_by_trace_point AS (
+  WITH within_bounds_by_track_point AS (
     SELECT
-        track_points.id  AS trace_point_id,
-        path_segments.id AS path_segment_id,
+        track_points.ogc_fid AS track_point_id,
+        path_segments.id     AS path_segment_id,
         row_number() OVER (
-            PARTITION BY trace_points.id
-            ORDER BY ST_Distance(path_segments.way, track_points.point) ASC
+            PARTITION BY track_points.ogc_fid
+            ORDER BY ST_Distance(
+                  path_segments.way,
+                  track_points.wkb_geometry
+            ) ASC
         ) as row_number
     FROM track_points
     INNER JOIN path_segments ON (
-        ST_DWithin(path_segments.way, track_points.point, 20)
+        ST_DWithin(
+                path_segments.way,
+                track_points.wkb_geometry,
+                20
+        )
     )
   )
   SELECT path_segment_id, COUNT(*) as num_hits
-  FROM within_bounds_by_trace_point
+  FROM within_bounds_by_track_point
   WHERE row_number = 1
   GROUP BY 1;
 
